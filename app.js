1ce0776ab1b25be37b2b1d101994cce3f1d8e06d
Sanchit <sanchitbhadgal36@gmail.com>
2021-12-31 11:10:30 +0530

    added user routes and controller

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const placesRouter = require("./routes/places-router");
-const HttpError = require("./models/http-error");
+const placesRouter = require('./routes/places-router');
+const usersRouter = require('./routes/users-router');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/places", placesRouter);
+app.use('/api/places', placesRouter);
+app.use('/api/users', usersRouter);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
@@ -21,7 +23,7 @@ app.use((error, req, res, next) => {
   }
 
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 app.listen(5000);
---
+++ controllers/users-controller.js
@@ -0,0 +1,51 @@
+const { v4: uuid } = require('uuid');
+const HttpError = require('../models/http-error');
+
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Sanchit Bhadgal',
+    email: 'sanchit@gmail.com',
+    password: 'abc',
+  },
+];
+
+exports.getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+exports.signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError('Could not create user, email already exists', 422);
+  }
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      'Could not identify user, credentials seems to be wrong',
+      401
+    );
+  }
+
+  res.status(200).json({ message: 'Logged in!' });
+};
